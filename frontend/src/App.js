4876ace4bbd70b97de26edb82d88375d444bbe79
nikhilmp448 <nikhilmp448@gmail.com>
2023-08-26 20:10:14 +0530

    dev stage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Registration from './components/Registration';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -9,18 +9,12 @@ function App() {
 
     return (
         <Router>
-            <Switch>
-                <Route path="/register" component={Registration} />
-                <Route path="/login">
-                    <Login setToken={setToken} />
-                </Route>
-                <ProtectedRoute
-                    path="/protected"
-                    component={() => <div>Protected Content</div>}
-                    token={token}
-                />
-                <Route path="/" component={() => <div>Home</div>} />
-            </Switch>
+            <Routes>
+                <Route path="/register" element={<Registration />} />
+                <Route path="/login" element={<Login setToken={setToken} />} />
+                <Route path="/protected" element={<ProtectedRoute token={token} />} />
+                <Route path="/" element={<div>Home</div>} />
+            </Routes>
         </Router>
     );
 }
