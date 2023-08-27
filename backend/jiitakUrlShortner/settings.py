1fc0b93ca7c8569f7cf7e2796d2c0eeed17db02c
nikhilmp448 <nikhilmp448@gmail.com>
2023-08-27 15:27:16 +0530

    final commit

--- backend/jiitakUrlShortner/settings.py
+++ backend/jiitakUrlShortner/settings.py
@@ -125,7 +125,7 @@ USE_I18N = True
 USE_TZ = True
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,45 +10,6 @@ import PublicRoute from './components/PublicRoute'
 
 function App() {
 
-  const [access, setAccessToken] = useState(localStorage.getItem('token') || '');
-  const [refresh, setRefreshToken] = useState(localStorage.getItem('refresh') || '');
-  const [accessTokenExpiry, setAccessTokenExpiry] = useState(localStorage.getItem('accessTokenExpiry') || 0);
-
-  const isAccessTokenExpired = () => {
-    return Date.now() > accessTokenExpiry;
-  };
-  const refreshAccessToken = async () => {
-    try {
-      const response = await fetch('http://localhost:8000/token/refresh/', {
-        method: 'POST',
-        body: JSON.stringify({ refresh }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setAccessToken(data.access);
-        setAccessTokenExpiry(Date.now() + data.expiresIn * 1000); // Set new expiration time
-        localStorage.setItem('token', data.access);
-        localStorage.setItem('refresh', data.refresh);
-
-        localStorage.setItem('accessTokenExpiry', accessTokenExpiry);
-      } else {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-      }
-    } catch (error) {
-      console.error('Error refreshing access token:', error.message);
-    }
-  };
-  useEffect(() => {
-    if (isAccessTokenExpired() && refresh) {
-      refreshAccessToken();
-    }
-  }, [access, refresh]);
-
   return (
     <BrowserRouter>
       <Routes>
